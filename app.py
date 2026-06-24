5fc61f990835aa8153cadb1069726474e5d7c47d
Balu <tbaludss@gmail.com>
2026-06-24 16:01:31 +0530

    feat: add create-task functionality

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-tasks = [] 
-print("Hellow world! ")
+tasks = []
+
+def create_task(title):
+    task = {
+        "title": title,
+        "status": "todo"
+    }
+    tasks.append(task)
+
+create_task("Study Git")
+print(tasks)
